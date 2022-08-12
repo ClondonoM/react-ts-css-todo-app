1e1ed7fe4358725492f354a97b50d0edbe3d9a38
Carlos Londono <c.londono.m@gmail.com>
2022-08-11 21:04:15 -0500

    adding model ts

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,9 +1,18 @@
 import styles from '../styles/InputField.module.css';
 
-const InputField = () => {
+interface Props { 
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+  handleAdd: (e:React.FormEvent) => void;
+}
+
+const InputField = ({todo, setTodo,handleAdd}:Props) => {
   return (
-    <form className={styles.input}>
-      <input type="input" placeholder="Enter a task" className={styles.input__box} />
+    <form className={styles.input} onSubmit={handleAdd}>
+      <input type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task" className={styles.input__box} />
       <button type="submit" className={styles.input__submit}>Go</button>
     </form>
   )
