253f2b7a035cbb8db82f19896b2e29a713c52e83
Carlos Londono <c.londono.m@gmail.com>
2022-08-14 10:00:43 -0500

    todo done functionality in SigleTodo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -3,18 +3,31 @@ import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import {MdDone} from 'react-icons/md'
 import styles from '../styles/SingleTodo.module.css'
 
+
 type Props = {
   todo: Todo,
   todos: Todo[],
   setTodos:React.Dispatch<React.SetStateAction<Todo[]>>
 }
 
-
-
-const SingleTodo = ({todo, todos, setTodos}:Props) => {
+const SingleTodo:React.FC<Props> = ({todo, todos, setTodos}:Props) => {
+  
+  const handleDone = (id:number) => {
+    setTodos(todos.map((todo) => todo.id === id ? { ...todo, isDone: !todo.isDone } : todo))
+          
+  }
+  
+  const styleDone: string = todo.isDone ? 
+      (`${styles.todos_sigle_text_done}`)
+     :
+    (`${styles.todos_sigle_text}`)
+  
+  
   return (
-    <form className={styles.todos_single}>
-      <span className={styles.todos_sigle_text}>{todo.todo}</span>
+    <form className={`${styles.todos_single} ${styleDone} 
+     
+    `}
+      >{todo.todo}
       <div>
         <span className={styles.icon}>
           <AiFillEdit/>
@@ -22,7 +35,7 @@ const SingleTodo = ({todo, todos, setTodos}:Props) => {
         <span className={styles.icon}>
           <AiFillDelete/>
         </span>
-        <span className={styles.icon}>
+        <span className={styles.icon} onClick={()=>handleDone(todo.id)}>
           <MdDone/>
         </span>
       </div>
